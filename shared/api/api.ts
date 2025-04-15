d2ddc9e07b162c84604449dc124de915672cf45f
Andrei <andrei18makrushin@gmail.com>
2025-04-15 21:36:51 +0500

    chore: read me and delete old api

--- shared/api/api.ts
+++
@@ -1,4 +0,0 @@
-export const API_list = 'https://api.anilibria.tv/v3/title/updates?page='
-export const API_anime = 'https://api.anilibria.tv/v3/title?id='
-export const API_search = 'https://api.anilibria.tv/v3/title/search?search='
-export const API_SIMILAR_ANIME = 'https://api.anilibria.tv/v3/title/search?genres='
