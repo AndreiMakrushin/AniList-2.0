6633db4304715ba975bb38383cb38f63d413a5a6
Andrei <andrei18makrushin@gmail.com>
2025-04-18 02:38:52 +0500

    fix: hot fix skeleton

--- pages/home/components/HomePage.vue
+++ pages/home/components/HomePage.vue
@@ -46,9 +46,9 @@ const goPageAnime = (code: string) => {
       ></AnimeCard>
     </AnimeGrid>
 
-    <AnimeGrid v-else><AnimeGridSkeleton v-for="i in 10" :key="i" /> </AnimeGrid>
+    <AnimeGrid v-else-if="isLoading"><AnimeGridSkeleton v-for="i in 10" :key="i" /> </AnimeGrid>
 
-    <Spinner v-if="isLoading && aniList" />
+    <Spinner v-if="!aniList && isLoading" />
 
     <div class="flex w-full h-full items-center justify-center">
       <img
--- shared/ui/skeletons/AnimeGridSkeleton.vue
+++ shared/ui/skeletons/AnimeGridSkeleton.vue
@@ -1,21 +1,9 @@
 <template>
   <div class="flex flex-col gap-3 w-full">
-    <!-- Скелетон изображения -->
-    <div 
-      class="
-        w-full rounded-[15px] bg-gray-200 dark:bg-gray-700
-        relative overflow-hidden
-        before:absolute before:inset-0 before:w-full before:h-full
-        before:bg-[length:200%_100%]
-        before:bg-gradient-to-r
-        before:from-gray-200 before:via-gray-300 before:to-gray-200
-        dark:before:from-gray-700 dark:before:via-gray-600 dark:before:to-gray-700
-        before:animate-shimmer
-        aspect-[3/4.248]
-      "
+    <div
+      class="w-full rounded-[15px] bg-gray-200 dark:bg-gray-700 relative overflow-hidden before:absolute before:inset-0 before:w-full before:h-full before:bg-[length:200%_100%] before:bg-gradient-to-r before:from-gray-200 before:via-gray-300 before:to-gray-200 dark:before:from-gray-700 dark:before:via-gray-600 dark:before:to-gray-700 before:animate-shimmer aspect-[3/4.248]"
     ></div>
 
-    <!-- Скелетон текста -->
     <div class="w-full h-6 rounded-[15px] bg-gray-200 dark:bg-gray-700"></div>
   </div>
 </template>
